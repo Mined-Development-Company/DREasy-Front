811c53c4a9a086928c3f50cccd76e0b613177ab1
ldvndev <ldvndev@gmail.com>
2023-11-18 14:53:17 -0300

    feat: add component login

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
+
 import { Home } from '../pages';
+import { Login } from '../components/login/Login';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
