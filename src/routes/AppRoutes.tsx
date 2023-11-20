811076588d2c9744359e4feee4f3028345d04472
ldvndev <ldvndev@gmail.com>
2023-11-20 20:39:09 -0300

    feat: add register

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from '../pages';
+import { Register } from '../components/register/Register';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
