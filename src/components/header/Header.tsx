9983deb8b8b86a9a7f19546480fc86bdc71b30d1
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-12-15 00:02:48 -0300

    feat: add header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,24 @@
-import { UseExample } from '../../context';
+import { Link } from "react-router-dom";
+import { IoSettingsSharp, IoNotifications } from 'react-icons/io5'
 
 export const Header = () => {
-  const { value, setValue } = UseExample();
   return (
-    <header className="h-screen flex justify-center items-center gap-6">
-      <button
-        onClick={() => setValue(value + 1)}
-        className="px-12 py-2 font-semibold text-white rounded-full bg-blue-800"
-      >
-        add
+    <nav className="flex items-center text-grayish-green gap-2 sm:gap-5 w-fit">
+      <div className="flex items-center">
+        <span className="w-7 h-7 sm:w-9 sm:h-9 mr-2 text-xs sm:text-sm border-2 rounded-full flex items-center justify-center font-medium bg-red-300 bg-white border-green-500 select-none">
+          US
+        </span>
+
+        <span className="text-sm sm:text-xl">Usuário</span>
+      </div>
+
+      <Link aria-label="Configurações" className="p-1" to="/settings">
+        <IoSettingsSharp />
+      </Link>
+
+      <button aria-label="Notificações" className="p-1" type="button">
+        <IoNotifications />
       </button>
-      <p className="text-xl font-semibold">{value}</p>
-    </header>
+    </nav>
   );
 };
