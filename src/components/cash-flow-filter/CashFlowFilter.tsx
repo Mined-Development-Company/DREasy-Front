92f49549fa0cf751847a4eb4b927d1c1b69dc693
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-12-23 11:53:32 -0300

    feat: add error messages on cash flow filter

--- src/components/cash-flow-filter/CashFlowFilter.tsx
+++ src/components/cash-flow-filter/CashFlowFilter.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { Select, Option } from "../../forms";
+import { Select, Option, ErrorMessage } from "../../forms";
 import { BaseLayout } from "../../layouts"
 import { Button } from "../ui";
 
@@ -26,32 +26,43 @@ export const CashFlowFilter = () => {
         onSubmit={onSubmit}
         className="grid grid-rows-2 grid-cols-3 gap-2 sm:gap-4 justify-between sm:flex"
       >
-        <Select
-          {...register('exibition')}
-          placeholder="Exibição"
-          classes={{ container: 'max-w-xs w-full' }}
-        >
-          <Option value="value-a">option-a</Option>
-          <Option value="value-b">option-b</Option>
-        </Select>
+        <div className="w-full max-w-xs">
+          <Select
+            {...register('exibition')}
+            placeholder="Exibição"
+          >
+            <Option value="value-a">option-a</Option>
+            <Option value="value-b">option-b</Option>
+          </Select>
 
-        <Select
-          {...register('month')}
-          placeholder="Mês"
-          classes={{ container: 'max-w-xs w-full' }}
-        >
-          <Option value="value-a">option-a</Option>
-          <Option value="value-b">option-b</Option>
-        </Select>
+          <ErrorMessage>{errors.exibition?.message}</ErrorMessage>
+        </div>
 
-        <Select
-          {...register('year')}
-          placeholder="Ano"
-          classes={{ container: 'max-w-xs w-full' }}
-        >
-          <Option value="value-a">option-a</Option>
-          <Option value="value-b">option-b</Option>
-        </Select>
+        <div className="w-full max-w-xs">
+          <Select
+            {...register('month')}
+            placeholder="Mês"
+          >
+            <Option value="value-a">option-a</Option>
+            <Option value="value-b">option-b</Option>
+          </Select>
+
+          <ErrorMessage>{errors.month?.message}</ErrorMessage>
+        </div>
+
+      
+        <div className="w-full max-w-xs">
+          <Select
+            {...register('year')}
+            placeholder="Ano"
+          >
+            <Option value="value-a">option-a</Option>
+            <Option value="value-b">option-b</Option>
+          </Select>
+
+          <ErrorMessage>{errors.year?.message}</ErrorMessage>
+        </div>
+
 
         <Button
           type="submit"
