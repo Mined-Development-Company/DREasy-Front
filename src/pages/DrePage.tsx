f263ee72c3cca39f32e760c7d2f7e8775e983ec6
devrogerinho <luaansilva11@gmail.com>
2024-01-13 17:00:55 -0500

    :lipstick: Feat: add table for cash-flow and DRE

--- src/pages/DrePage.tsx
+++ src/pages/DrePage.tsx
@@ -2,11 +2,15 @@
 import { LayoutDualPanel } from '../layouts';
 // Components
 import { PageIdentifier } from '../components';
+import { CashFlowFilter } from '../components/cash-flow-filter/CashFlowFilter';
+import TableDre from '../components/table-dre/TableDre';
 
 export const DrePage = () => {
   return (
     <LayoutDualPanel>
       <PageIdentifier title="DRE" />
+      <CashFlowFilter />
+      <TableDre />
     </LayoutDualPanel>
   );
 };
